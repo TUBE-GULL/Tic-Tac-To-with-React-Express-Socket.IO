aa6140369b631761b942b327136e11f15ce4ea9d
TUBE-GULL <ezapis11@gmail.com>
2024-04-04 12:20:08 +0300

    0.0.54

--- client/src/modules/game/components/chat/modules/Message.jsx
+++ client/src/modules/game/components/chat/modules/Message.jsx
@@ -1,12 +1,12 @@
 function Messages(props) {
    const { Nickname, sms, index } = props;
    return (
-      <div className="messagerecipient">
-         <div className={sms.userName === Nickname.Nickname ? 'messagesender' : 'messagerecipient'} key={index}>
-            <h1>{sms.userName}</h1>
-            <p>{sms.message}</p>
-         </div>
+
+      <div className={sms.userName === Nickname ? 'messageSender' : 'messageGet'} key={index}>
+         <h1>{sms.userName}</h1>
+         <p>{sms.message}</p>
       </div>
+
    );
 }
 
